a1debdc5890f93e42aede7135217fb4d206f5c17
kabindrr <chaina_kabindra@yahoo.com>
2024-06-01 10:25:12 +1000

    css in react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { useState } from "react";
+import { UserProfile } from "./component/UserComponent";
 
 function App() {
-  const [counter, setCounter] = useState(100);
-
   return (
     <>
-      <div>
-        <h1 className="d-flex align-item-center justify-content-center">
-          This is my Counter Clock
-        </h1>
-        <div>{counter}</div>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
-        <button onClick={() => setCounter(counter - 1)}>-</button>
+      <div
+        className="user-page"
+        style={{ background: "black", color: "wheat" }}
+      >
+        <UserProfile />
       </div>
     </>
   );
--- src/component/UserComponent.jsx
+++ src/component/UserComponent.jsx
@@ -1,18 +1,22 @@
-export const Profession = ({ nameProps, addProps, salaryProps }) => {
+export const UserProfile = () => {
   return (
-    <>
-      <h2>
-        {nameProps} is a Full Stack Developer from {addProps} Full stack
-        Developer estimated salary is {salaryProps}
-      </h2>
-    </>
-  );
-};
-
-export const Address = () => {
-  return (
-    <>
-      <h3>NSW,Sydney</h3>
-    </>
+    <div>
+      <div className="card">
+        <div className="img">
+          <img src="https://randomuser.me/api/portraits/women/28.jpg" alt="" />
+        </div>
+        <div className="info">
+          <h1>Kabindra Ranabhat</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est magnam
+            at, dolorem architecto fugiat quisquam sint officia? Fuga,
+            asperiores. Dignissimos soluta eligendi iste? Distinctio obcaecati
+            voluptas et enim, repellendus necessitatibus hic illum repudiandae,
+            similique aut, dignissimos asperiores non incidunt consequuntur nemo
+            at. Cum ipsa iure non qui officiis, fugiat ea?
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
